07828244bc52d600b2e10d3af4b8d6bf7770de2e
Kirill Simin <kirill@kirillsimin.com>
2018-08-27 15:18:46 +0000

    adds login test, updates factory

--- tests/Feature/Api/UserTest.php
+++ tests/Feature/Api/UserTest.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class UserTest extends TestCase
 {
-    use RefreshDatabase, WithoutMiddleware;
+    use RefreshDatabase;
 
     /** @test */
     public function new_user_can_register()
@@ -17,6 +17,22 @@ class UserTest extends TestCase
             'name' => 'Test User',
             'email' => 'test@example.org',
             'password' => '123456'
+        ]);
+
+        $this->assertDatabaseHas('users', [
+            'email' => 'test@example.org'
+        ]);
+        $response->assertStatus(200)->assertJsonStructure(['token']);
+    }
+
+    /** @test */
+    public function user_can_login()
+    {
+        $user = factory(\App\Models\User::class)->create();
+
+        $response = $this->json('POST', '/api/login', [
+            'email' => $user->email,
+            'password' => 'secret'
         ]);
 
         $response->assertStatus(200)->assertJsonStructure(['token']);
